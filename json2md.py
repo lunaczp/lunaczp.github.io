808bf5221a5eb7cd803435a57c1fddfb376099f1
Lunac <moon.zhp@gmail.com>
2024-05-24 20:56:42 +0800

    add notion md

---
+++ json2md.py
@@ -0,0 +1,32 @@
+import json
+import os
+import sys
+
+
+class Json2Md(object):
+
+    def toMd(self, node, level=0):
+        if node is None:
+            return
+        
+        link = f"[{node['title']}](https://www.notion.so/lunaczp/{node['id'].replace('-', '')})"
+        print(" " * level, "-", link)
+
+        if node.get("children") is None:
+            return
+
+        for child in node["children"]:
+            self.toMd(child, level + 1)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python buildNotion.py <jsonDataFile>")
+        exit(1)
+
+    jsonFile = sys.argv[1]
+    with open(jsonFile, "r") as f:
+        jsonData = json.load(f)
+
+    j = Json2Md()
+    j.toMd(jsonData)
