c70b1711acb9742071a68bb10315d1f004be1806
Lunac <moon.zhp@gmail.com>
2024-05-25 15:17:49 +0800

    fix level

--- json2md.py
+++ json2md.py
@@ -12,12 +12,16 @@ class Json2Md(object):
         if node["type"] == "child_page":
             link = f"[{node['title']}](https://www.notion.so/lunaczp/{node['id'].replace('-', '')})"
             print("  " * level, "-", link)
+            nextLevel = level + 1
+        else:
+            #不打印，则level也不变，保证对齐
+            nextLevel = level
 
         if node.get("children") is None:
             return
 
         for child in node["children"]:
-            self.toMd(child, level + 1)
+            self.toMd(child, nextLevel)
 
 
 if __name__ == "__main__":
